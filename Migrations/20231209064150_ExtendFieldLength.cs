848ea870e5f41311099e43357dbdcfd83e99e4ec
Ferdinand Sukhoi <ferdinandsukhoi@outlook.com>
2023-12-09 14:43:05 +0800

    fix field length issue

---
+++ Migrations/20231209064150_ExtendFieldLength.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SvcService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendFieldLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Services",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Services",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)",
+                oldMaxLength: 64)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceId",
+                table: "Interfaces",
+                type: "varchar(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CalleeIdSuffix",
+                table: "Dependencies",
+                type: "varchar(64)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)",
+                oldMaxLength: 32)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CalleeServiceId",
+                table: "Dependencies",
+                type: "varchar(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CallerIdSuffix",
+                table: "Dependencies",
+                type: "varchar(64)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)",
+                oldMaxLength: 32)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CallerServiceId",
+                table: "Dependencies",
+                type: "varchar(32)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Services",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)",
+                oldMaxLength: 32)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Services",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)",
+                oldMaxLength: 32)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceId",
+                table: "Interfaces",
+                type: "varchar(64)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CalleeIdSuffix",
+                table: "Dependencies",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CalleeServiceId",
+                table: "Dependencies",
+                type: "varchar(64)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CallerIdSuffix",
+                table: "Dependencies",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(64)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CallerServiceId",
+                table: "Dependencies",
+                type: "varchar(64)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(32)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
